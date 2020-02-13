57a37f2ddf293d65d7584dbbdbb1b3d3eae51208
Maigal <matiasigalmeida@gmail.com>
2020-02-13 14:42:37 -0300

    Updated the way scripts and styles are generated

--- templates/base.js
+++ templates/base.js
@@ -10,21 +10,33 @@ module.exports.getFiles = function (name, bootstrap, jquery, threejs) {
 }
 
 function getHtml(name, bootstrap, jquery, threejs) {
+
+  let scripts = [];
+  let styles = [];
+  if (bootstrap) {
+    scripts = [...scripts, ...versions.bootstrap.scripts]
+    styles = [...styles, ...versions.bootstrap.styles]
+  } else if (jquery) {
+    scripts = [...scripts, ...versions.jquery.scripts]
+  }
+
+  if (threejs) {
+    scripts = [...scripts, ...versions.threejs.scripts]
+  }
+
   return `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-  ${bootstrap ? versions.bootstrap.styles.map(el => `${el}\n`): ''}
+  ${styles.join('\n')}
   <link rel="stylesheet" type="text/css" href="${name}.css">
   <title>${name}</title>
 </head>
 <body>
-${(jquery && !bootstrap) ? versions.jquery.scripts.join('\n'): ''}
-${bootstrap ? versions.bootstrap.scripts.join('\n'): ''}
-${threejs ? versions.threejs.scripts.join('\n'): ''}
-<script src="${name}.js"></script>
+  ${scripts.join('\n')}
+  <script src="${name}.js"></script>
 </body>
 </html>
 `
