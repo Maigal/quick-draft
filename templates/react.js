e721eac7918cffcff84a5788267adb92da7ee2cd
Maigal <matiasigalmeida@gmail.com>
2020-02-19 21:23:39 -0300

    React basic package json

---
+++ templates/react.js
@@ -0,0 +1,15 @@
+module.exports.package = {
+    dependencies: [
+        'react',
+        'react-dom'
+    ],
+    devDependencies: [
+        'react-scripts'
+    ],
+    scripts: {
+        "start": "react-scripts start",
+        "build": "react-scripts build",
+        "test": "react-scripts test --env=jsdom",
+        "eject": "react-scripts eject"
+    }
+}
